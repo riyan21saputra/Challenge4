5383df85cb2c0e9a5ee68fd8e3d1187bc2ec89f7
riyan21saputra <abunyakhadijah@gmail.com>
2022-05-25 17:17:06 +0700

    revisi 1

--- app/src/main/java/id/co/riyan/challenge4/MainActivity.kt
+++ app/src/main/java/id/co/riyan/challenge4/MainActivity.kt
@@ -1,5 +1,6 @@
 package id.co.riyan.challenge4
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,24 +9,13 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-        val batu = 0
-        val kertas = 1
-        val gunting = 2
-
-        val pemenang = findViewById<View>(R.id.view_result)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val iconbatu = findViewById<ImageView>(R.id.view_batu)
-        val iconbatu2 = findViewById<ImageView>(R.id.view_batu2)
-        val iconkertas = findViewById<ImageView>(R.id.view_kertas)
-        val iconkertas2 = findViewById<ImageView>(R.id.view_kertas2)
-        val icongunting = findViewById<ImageView>(R.id.view_gunting)
-        val icongunting2 = findViewById<ImageView>(R.id.view_gunting2)
-
-        val pemenang = findViewById<View>(R.id.view_result)
+        lateinit var iconbatu : ImageView
+        lateinit var iconbatu2 : ImageView
+        lateinit var iconkertas : ImageView
+        lateinit var iconkertas2 : ImageView
+        lateinit var icongunting : ImageView
+        lateinit var icongunting2 : ImageView
+        lateinit var pemenang : ImageView
 
         // -1 kosong, 0 batu, 1 kertas, 2 gunting
         var iconterpilih1 = -1
@@ -34,6 +24,22 @@ class MainActivity : AppCompatActivity() {
         val batu = 0
         val kertas = 1
         val gunting = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        iconbatu = findViewById<ImageView>(R.id.view_batu)
+        iconbatu2 = findViewById<ImageView>(R.id.view_batu2)
+        iconkertas = findViewById<ImageView>(R.id.view_kertas)
+        iconkertas2 = findViewById<ImageView>(R.id.view_kertas2)
+        icongunting = findViewById<ImageView>(R.id.view_gunting)
+        icongunting2 = findViewById<ImageView>(R.id.view_gunting2)
+
+        pemenang = findViewById<ImageView>(R.id.view_result)
+
+        val reset = findViewById<ImageView>(R.id.view_reset)
+
 
         iconbatu.setOnClickListener {
             Log.d(MainActivity::class.java.simpleName, "batu terpilih")
@@ -70,11 +76,41 @@ class MainActivity : AppCompatActivity() {
             iconterpilih2 = 2
 
         }
-
     }
 
-    private fun perlombaan(){
-        if(batu >=0 && kertas >=0 ){
-            pemenang.setBackgroundColor(R.id.view_kertas)
+    private fun selectViewPertama(view: ImageView, pilihan: Int){
+        iconterpilih1 = pilihan
+        clearVIewPertama()
+        view.setImageResource(R.drawable.ic_baseline_check_24)
+        hasil()
+    }
+
+    private fun selectViewKedua(view: ImageView, pilihan: Int){
+        iconterpilih1 = pilihan
+        clearVIewKedua()
+        view.setImageResource(R.drawable.ic_baseline_check_24)
+        hasil()
+    }
+
+    private fun clearVIewPertama(){
+        iconbatu.setImageResource(0)
+        icongunting.setImageResource(0)
+        iconkertas.setImageResource(0)
+    }
+    private fun clearVIewKedua(){
+        iconbatu2.setImageResource(0)
+        icongunting2.setImageResource(0)
+        iconkertas2.setImageResource(0)
+    }
+
+    private fun hasil(){
+        if(iconterpilih1 >=0 && iconterpilih2 >=0){
+            if(iconterpilih1 == batu && iconterpilih2 == kertas || iconterpilih1 == kertas && iconterpilih2 == gunting || iconterpilih1 == gunting && iconterpilih2 == batu){
+                pemenang.setBackgroundResource(R.drawable.ic_pemain2);
+            }else if(iconterpilih1 == batu && iconterpilih2 == gunting || iconterpilih1 == kertas && iconterpilih2 == batu || iconterpilih1 == gunting && iconterpilih2 == kertas){
+                pemenang.setBackgroundResource(R.drawable.ic_pemain1);
+            }
         }
+    }
+
 }
